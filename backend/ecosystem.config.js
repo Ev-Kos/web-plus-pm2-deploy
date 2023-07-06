35eaa7fd5a4a24ef235373e426ae833b6a15137c
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-07-06 11:19:53 +0300

    fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -31,8 +31,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build && pm2 kill && pm2 start ecosystem.config.js && pm2 save',
+      'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': 'cd backend && npm i && npm run build && pm2 start',
     },
   },
 };
